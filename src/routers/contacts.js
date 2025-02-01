5c21340cf0492e4fda419c70ed22e989ea70428f
Keemply <144055348+Keemply@users.noreply.github.com>
2025-02-01 17:05:00 +0200

    fixed update schema

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,7 +8,10 @@ import {
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { createContactsSchema } from '../validation/contacts.js';
+import {
+  createContactsSchema,
+  updateContactsSchema,
+} from '../validation/contacts.js';
 import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
@@ -26,7 +29,7 @@ router.post(
 router.patch(
   '/contacts/:contactId',
   isValidId,
-  validateBody(createContactsSchema),
+  validateBody(updateContactsSchema),
   ctrlWrapper(patchContactController),
 );
 router.delete(
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -6,3 +6,10 @@ export const createContactsSchema = Joi.object({
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
+export const updateContactsSchema = Joi.object({
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
+  email: Joi.string().min(3).max(20),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+});
