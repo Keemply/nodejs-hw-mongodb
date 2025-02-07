1f3d78184f06ea1f4ae81272de39046289e2d4fc
Keemply <144055348+Keemply@users.noreply.github.com>
2025-02-07 02:07:00 +0200

    wip

--- src/db/models/contacts.js
+++ src/db/models/contacts.js
@@ -22,6 +22,10 @@ const contactsSchema = new Schema(
       default: 'personal',
       required: true,
     },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'users',
+    },
   },
   {
     timestamps: true,
--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-import contactsRouter from './routers/contacts.js';
+import router from './routers/index.js';
 import { getEnvVar } from './utils/getEnvVar.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -13,7 +14,7 @@ export const setupServer = () => {
 
   app.use(express.json());
   app.use(cors());
-
+  app.use(cookieParser());
   app.use(
     pino({
       transport: {
@@ -21,7 +22,7 @@ export const setupServer = () => {
       },
     }),
   );
-  app.use(contactsRouter);
+  app.use(router);
   app.use('*', notFoundHandler);
   app.use(errorHandler);
   app.listen(PORT, () => {
